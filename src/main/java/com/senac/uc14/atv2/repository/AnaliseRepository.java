7d550d51975456d1659efc473f6b5ee2d27a5ab4
devjf <djfcoder@outlook.com>
2025-09-29 23:12:41 -0300

    fix: move package path to correct path

--- src/main/java/com/senac/uc14/atv2/repository/AnaliseRepository.java
+++ src/main/java/com/senac/uc14/atv2/repository/AnaliseRepository.java
@@ -1,4 +1,4 @@
-package com.senac.uc14.repository;
+package com.senac.uc14.atv2.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
