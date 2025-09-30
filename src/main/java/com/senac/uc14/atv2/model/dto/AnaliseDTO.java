04d0fea4b3df01e4f2a71cf08117ba38351249e6
devjf <djfcoder@outlook.com>
2025-09-29 23:14:22 -0300

    feat: implements analise service and dto with validations

--- src/main/java/com/senac/uc14/atv2/model/dto/AnaliseDTO.java
+++ src/main/java/com/senac/uc14/atv2/model/dto/AnaliseDTO.java
@@ -1,6 +1,19 @@
 package com.senac.uc14.atv2.model.dto;
 
-import com.senac.uc14.atv2.model.entity.Filme;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
-public record AnaliseDTO(Filme filme, String filmeAnalise, Integer nota) {
+public record AnaliseDTO(
+        @NotNull(message = "ID do filme é obrigatório")
+        Long filmeId,
+        @Size(min = 2, max = 500, message = "A análise deve ter entre 2 e 500 caracteres")
+        String filmeAnalise,
+        @NotNull(message = "Nota obrigatória")
+        @Min(0)
+        @Max(5)
+        Integer nota
+        ) {
+
 }
--- src/main/java/com/senac/uc14/atv2/service/AnaliseService.java
+++ src/main/java/com/senac/uc14/atv2/service/AnaliseService.java
@@ -2,6 +2,37 @@ package com.senac.uc14.atv2.service;
 
 import org.springframework.stereotype.Service;
 
+import com.senac.uc14.atv2.model.dto.AnaliseDTO;
+import com.senac.uc14.atv2.model.entity.Analise;
+import com.senac.uc14.atv2.model.entity.Filme;
+import com.senac.uc14.atv2.repository.AnaliseRepository;
+import com.senac.uc14.atv2.repository.FilmeRepository;
+
+import jakarta.validation.Valid;
+
 @Service
 public class AnaliseService {
+
+    private final AnaliseRepository analiseRepository;
+    private final FilmeRepository filmeRepository;
+
+    public AnaliseService(AnaliseRepository analiseRepository, FilmeRepository filmeRepository) {
+        this.analiseRepository = analiseRepository;
+        this.filmeRepository = filmeRepository;
+    }
+
+    public Analise analiseRegister(@Valid AnaliseDTO dto) {
+        // Buscar o filme pelo ID
+        Filme filme = filmeRepository.findById(dto.filmeId())
+                .orElseThrow(
+                        () -> new RuntimeException("Filme não encontrado com ID: " + dto.filmeId())
+                );
+        // Montar a análise
+        Analise entity = new Analise();
+        entity.setFilme(filme);
+        entity.setFilmeAnalise(dto.filmeAnalise());
+        entity.setNota(dto.nota());
+
+        return analiseRepository.save(entity);
+    }
 }
