0cccb9a94671df846624bec02ed9419eae1caae7
devjf <djfcoder@outlook.com>
2025-09-29 23:28:58 -0300

    feat: implements validations to filme dto

--- src/main/java/com/senac/uc14/atv2/model/dto/FilmeDTO.java
+++ src/main/java/com/senac/uc14/atv2/model/dto/FilmeDTO.java
@@ -1,4 +1,26 @@
 package com.senac.uc14.atv2.model.dto;
 
-public record FilmeDTO(String titulo, String sinopse, String genero, Integer anoLancamento) {
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record FilmeDTO(
+        @NotNull(message = "Titulo obrigatório")
+        @Size(max = 150, message = "Título muito longo")
+        String titulo,
+        @NotBlank(message = "Sinópse obrigatória")
+        @Size(min = 20, max = 500, message = "Sinopse: 20-500 caracteres")
+        String sinopse,
+        @NotBlank(message = "Gênero obrigatório")
+        @Pattern(regexp = "^(Ação|Drama|Comédia|Terror|Ficção Científica|Romance|Aventura|Animação)$",
+                flags = Pattern.Flag.CANON_EQ,
+                message = "Gênero deve ser: Ação, Drama, Comédia, Terror, Ficção Científica, Romance, Aventura ou Animação")
+        String genero,
+        @Min(value = 1888, message = "Ano inválido")
+        @Max(value = 2030, message = "Ano inválido")
+        Integer anoLancamento) {
+
 }
