f6dd05a5059948d2c45e5baea5ba6239e3f47a7e
devjf <djfcoder@outlook.com>
2025-10-01 00:12:14 -0300

    feat: implements filme service

--- src/main/java/com/senac/uc14/atv2/service/FilmeService.java
+++ src/main/java/com/senac/uc14/atv2/service/FilmeService.java
@@ -1,8 +1,39 @@
 package com.senac.uc14.atv2.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.senac.uc14.atv2.model.dto.FilmeDTO;
+import com.senac.uc14.atv2.model.entity.Filme;
+import com.senac.uc14.atv2.repository.FilmeRepository;
 
 @Service
 public class FilmeService {
 
+    private final FilmeRepository repository;
+
+    public FilmeService(FilmeRepository repository) {
+        this.repository = repository;
+    }
+
+    public Filme filmeRegister(FilmeDTO dto) {
+        Filme entity = new Filme();
+        entity.setTitulo(dto.titulo());
+        entity.setSinopse(dto.sinopse());
+        entity.setGenero(dto.genero());
+        entity.setAnoLancamento(dto.anoLancamento());
+
+        return repository.save(entity);
+    }
+
+    public List<Filme> filmesListAll() {
+        return repository.findAll();
+    }
+
+    public Filme getFilmeById(Long filmeId) {
+        return repository.findById(filmeId).orElseThrow(
+                () -> new RuntimeException("Filme não encontrado com ID: " + filmeId)
+        );
+    }
 }
