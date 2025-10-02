04d453dd640ca8bad8e98ad26cc8257de1268807
devjf <djfcoder@outlook.com>
2025-10-02 00:19:01 -0300

    fix: CORS errors

--- src/main/java/com/senac/uc14/atv2/controller/FilmeController.java
+++ src/main/java/com/senac/uc14/atv2/controller/FilmeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,8 @@ import com.senac.uc14.atv2.service.FilmeService;
 import jakarta.validation.Valid;
 
 @RestController
+// @CrossOrigin(origins = {"http://localhost:3000", "http://127.0.0.1:3000"})
+@CrossOrigin(origins = "*")
 @RequestMapping("/filmes")
 public class FilmeController {
 
@@ -39,6 +42,6 @@ public class FilmeController {
 
     @GetMapping("/detalhar/{id}")
     public ResponseEntity<Filme> mostrarDetalhesDoFilmePorId(@PathVariable Long id) {
-        return ResponseEntity.status(HttpStatus.FOUND).body(filmeService.getFilmeById(id));
+        return ResponseEntity.status(HttpStatus.OK).body(filmeService.getFilmeById(id));
     }
 }
